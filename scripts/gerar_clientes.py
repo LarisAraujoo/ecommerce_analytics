672202cfec39feb9c4dc71202c03a53c701c1906
Larissa Araujo <lari_araujo093@hotmail.com>
2026-03-19 19:50:55 -0300

    adicionando dados

---
+++ scripts/gerar_clientes.py
@@ -0,0 +1,18 @@
+import pandas as pd
+from faker import Faker
+
+fake = Faker("pt-BR")
+
+clientes = []
+
+for i in range(10):
+ cliente = {
+    "cliente_id": i,
+    "nome": fake.name(),
+    "cidade": fake.city(),
+    "estado": fake.state()
+}
+ clientes.append(cliente) 
+
+df = pd.DataFrame(clientes)
+df.to_csv("data/clientes.csv", index=False)
