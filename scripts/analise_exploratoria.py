46963e2810aa6ec18ae1449db8b896817ce70b18
Larissa Araujo <lari_araujo093@hotmail.com>
2026-03-29 11:20:55 -0300

    analises exploratorias de faturamento

--- scripts/analise_exploratoria.py
+++ scripts/analise_exploratoria.py
@@ -22,5 +22,33 @@ df_final = pd.merge(pedidos_itens, produtos, on="produto_id")
 
 df_final["faturamento"] = df_final["quantidade"] * df_final["preco"]
 
+# Faturamento total
 faturamento_total = df_final["faturamento"].sum()
 print("Faturamento total da empresa:", faturamento_total)
+
+# Ticket médio
+ticket_medio = df_final["faturamento"].mean()
+print("Ticket médio:", ticket_medio)
+
+# Total de pedidos
+total_pedidos = df_final["pedido_id"].nunique()
+print("Total de pedido:", total_pedidos)
+
+# Clientes únicos
+clientes_unicos = df_final["cliente_id"].nunique()
+print("Clientes únicos:", clientes_unicos)
+
+
+# Produtos mais vendidos
+produtos_mais_vendidos = (
+    df_final
+    .groupby("nome")["quantidade"]
+    .sum()
+    .sort_values(ascending=False)
+)
+
+print("Top 10 produtos mais vendidos:")
+print(produtos_mais_vendidos.head(10))
+
+df_final.to_csv("data/dataset_final.csv", index=False)
+print("Dataset final salvo em data/dataset_final.csv")
